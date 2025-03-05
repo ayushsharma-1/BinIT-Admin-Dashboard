49718b9327d44a78e35d2d9bd8c94b10e227f85b
ayushsharma-1 <ayushsharma18001@gmail.com>
2025-03-05 20:24:16 +0530

    fixing env issues

--- src/pages/list/ContactUS.jsx
+++ src/pages/list/ContactUS.jsx
@@ -57,31 +57,3 @@ const CarePlanList = () => {
 };
 
 export default CarePlanList;
-
-
-
-// import { ContactUsColumns } from "../../columns/columnData";
-// import { Datatable } from "../../components/datatable/Datatable";
-// import "./List.scss";
-
-// const CarePlanList = () => {
-//   return (
-//     <div className="records">
-//       <div className="topbar">
-//         <div className="recordstext">
-//           <span>Contact Us Reports</span>
-//         </div>
-//       </div>
-//       <hr />
-//       <div className="recordsbody">
-//         <Datatable
-//           db="careplans"
-//           columns={ContactUsColumns}
-//           className="datatable"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CarePlanList;
